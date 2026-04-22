08fa4fcc4adce5b3c060d2fdb9035a8d576320d4
Nazar Luchynin <luchnazar@gmail.com>
2026-04-22 20:37:55 +0300

    Update №4. Added New Data Types To Objects.cs

--- Functions_Storage/Functions/Objects.cs
+++ Functions_Storage/Functions/Objects.cs
@@ -1,39 +1,220 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace Functions_Storage;
 
-public class stringN
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct intN : IComparable, IComparable<intN>, IEquatable<intN>
 {
-    private char[] _v;
-    public int Length { get; private set; }
-    public stringN(string s) {
-        if (s == null) { _v = new char[0]; Length = 0; return; }
-        Length = s.Length;
-        _v = new char[Length];
-        for (int i = 0; i < Length; i++) _v[i] = s[i];
+    private readonly int _v;
+    public intN(int v) => _v = v;
+    public static implicit operator intN(int v) => new(v);
+    public static implicit operator int(intN v) => v._v;
+    public static intN operator +(intN a, intN b) => a._v + b._v;
+    public static intN operator -(intN a, intN b) => a._v - b._v;
+    public static intN operator *(intN a, intN b) => a._v * b._v;
+    public static intN operator /(intN a, intN b) => a._v / b._v;
+    public static intN operator %(intN a, intN b) => a._v % b._v;
+    public static intN operator ++(intN a) => new(a._v + 1);
+    public static intN operator --(intN a) => new(a._v - 1);
+    public static bool operator >(intN a, intN b) => a._v > b._v;
+    public static bool operator <(intN a, intN b) => a._v < b._v;
+    public static bool operator >=(intN a, intN b) => a._v >= b._v;
+    public static bool operator <=(intN a, intN b) => a._v <= b._v;
+    public static bool operator ==(intN a, intN b) => a._v == b._v;
+    public static bool operator !=(intN a, intN b) => a._v != b._v;
+    public static intN operator <<(intN a, int b) => a._v << b;
+    public static intN operator >>(intN a, int b) => a._v >> b;
+    public static intN operator &(intN a, intN b) => a._v & b._v;
+    public static intN operator |(intN a, intN b) => a._v | b._v;
+    public static intN operator ^(intN a, intN b) => a._v ^ b._v;
+    public static intN operator ~(intN a) => ~a._v;
+    public int CompareTo(intN other) => _v.CompareTo(other._v);
+    public int CompareTo(object obj) => obj is intN n ? _v.CompareTo(n._v) : 0;
+    public bool Equals(intN other) => _v == other._v;
+    public override bool Equals(object obj) => obj is intN n && Equals(n);
+    public override int GetHashCode() => _v;
+    public bool IsEven => _v % 2 == 0;
+    public bool this[int bit] => (_v & (1 << bit)) != 0;
+    public unsafe void WriteHex(char* buffer)
+    {
+        const string hexChars = "0123456789ABCDEF";
+        buffer[0] = '0';
+        buffer[1] = 'x';
+        for (int i = 0; i < 8; i++)
+        {
+            buffer[9 - i] = hexChars[(_v >> (i * 4)) & 0xF];
+        }
+    }
+    public static intN Parse(ReadOnlySpan<char> s) => new(int.Parse(s));
+    public static bool TryParse(ReadOnlySpan<char> s, out intN result)
+    {
+        bool success = int.TryParse(s, out int value);
+        result = new intN(value);
+        return success;
+    }
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct floatN : IComparable<floatN>, IEquatable<floatN>
+{
+    private readonly float _v;
+    public floatN(float v) => _v = v;
+    public static implicit operator floatN(float v) => new(v);
+    public static implicit operator float(floatN v) => v._v;
+    public static floatN operator +(floatN a, floatN b) => a._v + b._v;
+    public static floatN operator -(floatN a, floatN b) => a._v - b._v;
+    public static floatN operator *(floatN a, floatN b) => a._v * b._v;
+    public static floatN operator /(floatN a, floatN b) => a._v / b._v;
+    public static bool operator >(floatN a, floatN b) => a._v > b._v;
+    public static bool operator <(floatN a, floatN b) => a._v < b._v;
+    public static bool operator >=(floatN a, floatN b) => a._v >= b._v;
+    public static bool operator <=(floatN a, floatN b) => a._v <= b._v;
+    public static bool operator ==(floatN a, floatN b) => a._v == b._v;
+    public static bool operator !=(floatN a, floatN b) => a._v != b._v;
+    public int CompareTo(floatN other) => _v.CompareTo(other._v);
+    public bool Equals(floatN other) => _v == other._v;
+    public override bool Equals(object obj) => obj is floatN other && Equals(other);
+    public override int GetHashCode() => _v.GetHashCode();
+    public bool IsPositive => _v > 0;
+    public bool IsNegative => _v < 0;
+    public bool IsNearZero => Math.Abs(_v) < 0.0001f;
+    public unsafe intN ToRawInt()
+    {
+        float val = _v;
+        return *(intN*)&val;
+    }
+    public static floatN Parse(ReadOnlySpan<char> s) => new(float.Parse(s));
+    public static bool TryParse(ReadOnlySpan<char> s, out floatN result)
+    {
+        bool success = float.TryParse(s, out float value);
+        result = new floatN(value);
+        return success;
+    }
+    public override string ToString() => _v.ToString();
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct doubleN : IComparable<doubleN>, IEquatable<doubleN>, ISpanFormattable
+{
+    private readonly double _v;
+    public doubleN(double v) => _v = v;
+    public doubleN(intN v) => _v = (int)v;
+    public static implicit operator doubleN(double v) => new doubleN(v);
+    public static implicit operator double(doubleN v) => v._v;
+    public static implicit operator doubleN(intN v) => new doubleN(v);
+    public static doubleN PI => 3.1415926535897931;
+    public static doubleN E => 2.7182818284590451;
+    public static doubleN MaxValue => double.MaxValue;
+    public static doubleN MinValue => double.MinValue;
+    public static doubleN Epsilon => double.Epsilon;
+    public static doubleN NaN => double.NaN;
+    public static doubleN PositiveInfinity => double.PositiveInfinity;
+    public static doubleN NegativeInfinity => double.NegativeInfinity;
+    public static doubleN operator +(doubleN a, doubleN b) => a._v + b._v;
+    public static doubleN operator -(doubleN a, doubleN b) => a._v - b._v;
+    public static doubleN operator *(doubleN a, doubleN b) => a._v * b._v;
+    public static doubleN operator /(doubleN a, doubleN b) => a._v / b._v;
+    public static doubleN operator -(doubleN a) => -a._v;
+    public static bool operator >(doubleN a, doubleN b) => a._v > b._v;
+    public static bool operator <(doubleN a, doubleN b) => a._v < b._v;
+    public static bool operator >=(doubleN a, doubleN b) => a._v >= b._v;
+    public static bool operator <=(doubleN a, doubleN b) => a._v <= b._v;
+    public static bool operator ==(doubleN a, doubleN b) => a._v == b._v;
+    public static bool operator !=(doubleN a, doubleN b) => a._v != b._v;
+    public int CompareTo(doubleN other) => _v.CompareTo(other._v);
+    public bool Equals(doubleN other) => _v == other._v;
+    public override bool Equals(object obj) => obj is doubleN other && Equals(other);
+    public override int GetHashCode() => _v.GetHashCode();
+    public bool IsNaN => double.IsNaN(_v);
+    public bool IsInfinity => double.IsInfinity(_v);
+    public bool IsPositive => _v > 0;
+    public bool IsNegative => _v < 0;
+    public bool IsNearZero => Math.Abs(_v) < 1e-15;
+    public doubleN Abs() => Math.Abs(_v);
+    public doubleN Sqrt() => Math.Sqrt(_v);
+    public doubleN Round() => Math.Round(_v);
+    public doubleN Round(intN digits) => Math.Round(_v, (int)digits);
+    public doubleN Floor() => Math.Floor(_v);
+    public doubleN Ceiling() => Math.Ceiling(_v);
+    public doubleN Pow(doubleN power) => Math.Pow(_v, power._v);
+    public doubleN Sin() => Math.Sin(_v);
+    public doubleN Cos() => Math.Cos(_v);
+    public doubleN Tan() => Math.Tan(_v);
+    public unsafe long ToRawBits() { double val = _v; return *(long*)&val; }
+    public static doubleN Parse(ReadOnlySpan<char> s) => new(double.Parse(s));
+    public static bool TryParse(ReadOnlySpan<char> s, out doubleN result)
+    {
+        bool success = double.TryParse(s, out double value);
+        result = new doubleN(value);
+        return success;
+    }
+    public bool TryFormat(Span<char> destination, out int charsWritten, ReadOnlySpan<char> format, IFormatProvider provider)
+    => _v.TryFormat(destination, out charsWritten, format, provider);
+    public string ToString(string format, IFormatProvider formatProvider) => _v.ToString(format, formatProvider);
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public readonly struct charN : IComparable<charN>, IEquatable<charN>
+{
+    private readonly char _v;
+    public charN(char v) => _v = v;
+    public static implicit operator charN(char v) => new charN(v);
+    public static implicit operator char(charN v) => v._v;
+    public static implicit operator intN(charN v) => (int)v._v;
+    public static explicit operator charN(intN v) => (char)(int)v;
+    public static charN operator +(charN c, intN s) => (char)(c._v + (int)s);
+    public static charN operator -(charN c, intN s) => (char)(c._v - (int)s);
+    public static bool operator >(charN a, charN b) => a._v > b._v;
+    public static bool operator <(charN a, charN b) => a._v < b._v;
+    public static bool operator ==(charN a, charN b) => a._v == b._v;
+    public static bool operator !=(charN a, charN b) => a._v != b._v;
+    public int CompareTo(charN o) => _v.CompareTo(o._v);
+    public bool Equals(charN o) => _v == o._v;
+    public override bool Equals(object o) => o is charN c && Equals(c);
+    public override int GetHashCode() => _v.GetHashCode();
+    public override string ToString() => _v.ToString();
+    public bool IsDigit => char.IsDigit(_v);
+    public bool IsLetter => char.IsLetter(_v);
+    public bool IsUpper => char.IsUpper(_v);
+    public bool IsLower => char.IsLower(_v);
+    public bool IsWhiteSpace => char.IsWhiteSpace(_v);
+    public charN ToUpper() => char.ToUpper(_v);
+    public charN ToLower() => char.ToLower(_v);
+}
+
+public unsafe ref struct stringN
+{
+    private charN* _v;
+    private intN _l;
+    public intN Length => _l;
+    public stringN(charN* ptr, intN len) { _v = ptr; _l = len; }
+    public charN this[intN i] {
+        get => _v[(int)i];
+        set => _v[(int)i] = value;
     }
     public bool IsPalindrome {
         get {
-            for (int i = 0; i < Length / 2; i++)
-                if (_v[i] != _v[Length - 1 - i]) return false;
+            for (intN i = 0; i < _l / (intN)2; i++)
+                if (_v[(int)i] != _v[(int)(_l - (intN)1 - i)]) return false;
             return true;
         }
     }
-    public stringN Reverse() {
-        char[] a = new char[Length];
-        for (int i = 0; i < Length; i++) a[i] = _v[Length - 1 - i];
-        return new stringN(new string(a));
+    public void Reverse() {
+        for (intN i = 0; i < _l / (intN)2; i++) {
+            charN t = _v[(int)i];
+            _v[(int)i] = _v[(int)(_l - (intN)1 - i)];
+            _v[(int)(_l - (intN)1 - i)] = t;
+        }
     }
-    public override string ToString() => new string(_v);
-    public static implicit operator stringN(string s) => new stringN(s);
-    public static stringN operator +(stringN a, stringN b) {
-        char[] res = new char[a.Length + b.Length];
-        for (int i = 0; i < a.Length; i++) res[i] = a._v[i];
-        for (int i = 0; i < b.Length; i++) res[a.Length + i] = b._v[i];
-        return new stringN(new string(res));
+    public static bool operator ==(stringN a, stringN b) {
+        if (a._l != b._l) return false;
+        for (intN i = 0; i < a._l; i++) if (a[i] != b[i]) return false;
+        return true;
     }
+    public static bool operator !=(stringN a, stringN b) => !(a == b);
 }
 public class vectorN
 {
@@ -244,5 +425,72 @@ public class queueN<T>
         var q = new queueN<T>(a.Length);
         foreach (var x in a) q.Enqueue(x);
         return q;
+    }
+}
+
+
+public static unsafe class NCore
+{
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern IntPtr GetStdHandle(int nStdHandle);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool WriteConsoleW(IntPtr hConsoleOutput, void* lpBuffer, uint nNumberOfCharsToWrite, out uint lpNumberOfCharsWritten, void* lpReserved);
+    [DllImport("kernel32.dll", SetLastError = true)]
+    private static extern bool SetConsoleTextAttribute(IntPtr hConsoleOutput, ushort wAttributes);
+    private static readonly intN STD_OUT = (intN)(-11);
+    private static readonly IntPtr _hOut = GetStdHandle((int)STD_OUT);
+    public static readonly ushort RED = 0x0004;
+    public static readonly ushort GREEN = 0x000A;
+    public static readonly ushort GRAY = 0x0007;
+    public static void SetColor(ushort color) => SetConsoleTextAttribute(_hOut, color);
+    public static void WriteRaw(char* buffer, intN length) => WriteConsoleW(_hOut, buffer, (uint)(int)length, out _, null);
+    public static void WriteRaw(string s, intN length) { fixed (char* ptr = s) WriteConsoleW(_hOut, ptr, (uint)(int)length, out _, null); }
+    public static void Print(charN val, bool newLine = true) {
+        char* buf = stackalloc char[2];
+        buf[0] = (char)val;
+        intN len = 1;
+        if (newLine) { buf[1] = '\n'; len = 2; }
+        WriteRaw(buf, len);
+    }
+    public static void Print(intN val, bool newLine = true) {
+        char* buf = stackalloc char[12];
+        intN v = val, p = 0;
+        if (v == 0) buf[p++] = '0';
+        else {
+            if (v < 0) { buf[p++] = '-'; v = (intN)0 - v; }
+            char* t = stackalloc char[10];
+            intN tp = 0;
+            while (v > 0) { t[tp++] = (charN)(char)('0' + (int)(v % (intN)10)); v /= (intN)10; }
+            while (tp > 0) buf[p++] = (char)t[--tp];
+        }
+        if (newLine) buf[p++] = '\n';
+        WriteRaw(buf, p);
+    }
+    public static void Print(doubleN val, bool newLine = true) {
+        Span<char> s = stackalloc char[32];
+        if (val.TryFormat(s, out int c, default, null)) {
+            fixed (char* ptr = s) {
+                if (newLine) ptr[c++] = '\n';
+                WriteRaw(ptr, (intN)c);
+            }
+        }
+    }
+    public static void PrintHex(intN val, bool newLine = true) {
+        intN l = newLine ? (intN)11 : (intN)10;
+        char* b = stackalloc char[(int)l];
+        val.WriteHex(b);
+        if (newLine) b[10] = '\n';
+        WriteRaw(b, l);
+    }
+    public static void PrintRawBits(doubleN val) {
+        long bits = val.ToRawBits();
+        intN h = (intN)(int)(bits >> 32);
+        intN lw = (intN)(int)(bits & 0xFFFFFFFF);
+        char* b = stackalloc char[22];
+        h.WriteHex(b);
+        lw.WriteHex(b + 10);
+        b[10] = '_';
+        b[21] = '\n';
+        WriteRaw(b, (intN)22);
     }
 }
